d44d0730b1e7aa2e0c72c0cf1a6c32ccb9494d1b
kadir odacı <abdlkdrdci@gmail.com>
2025-05-25 15:01:35 +0200

    update

--- pages/1_Klant_Toevoegen.py
+++ pages/1_Klant_Toevoegen.py
@@ -1,13 +1,15 @@
 import streamlit as st
 
-st.title("👤 Gebruiker Toevoegen")
+st.title("👤 Klant Toevoegen")
 
 with st.form("user_form"):
-    name = st.text_input("Naam")
+    name = st.text_input("Voer de naam van de klant in")
     email = st.text_input("E-mail")
     plate = st.text_input("Kenteken", placeholder="Bijv. GGX11S")
 
     submitted = st.form_submit_button("Toevoegen")
     if submitted:
-        st.success(f"Gebruiker {name} met kenteken {plate} toegevoegd!")
+        st.success(f"Klant {name} met kenteken {plate} toegevoegd!")
         # Optional: Save to database or file here
+
+
